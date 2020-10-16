6e6844f09b8adc1d000003e9e23e5c6737ce5248
Karen Mendivil <karenlili14@gmail.com>
2020-10-16 16:39:41 -0700

    hasta endponint/mine

---
+++ service/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Blockchain from '../blockchain';
+
+const{HTTP_PORT = 3000} = process.env;
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/blocks',(req,res) =>{
+    res.json(blockchain.blocks);
+});
+
+app.post('/mine', (req, res) =>{
+    const { body: { data } } = req;
+    const block = blockchain.addBlock(data);
+
+    restart.json({
+        blocks: blockchain.blocks.length,
+        block,
+    })
+});
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Service HTTP: ${HTTP_PORT} funcionando...`);
+});
